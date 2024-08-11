006ae00f8cfa19a951fabbf2ba69ff980fc99bbc
Ian <ianmcclue@protonmail.com>
2024-08-12 00:57:00 +0100

    Yuhh

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,7 +25,7 @@ const m2dxOptions = {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://starfunnel.unfolding.io",
+  site: "https://peppas.com",
   output: "hybrid",
   adapter: env.NETLIFY ? netlify(): vercel(), // vercel() or netlify()
   integrations: [
